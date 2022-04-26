0a23052e9ceaedb2eea46697e22f667d537c010b
iphyokafor <sandraifeoma22@gmail.com>
2022-04-26 09:53:33 +0100

    ft(deploy contract to testnet)

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -5,7 +5,7 @@ async function main() {
   const nft = await NFT.deploy(
     "Web3 Group 3 ladies",
     "WGL",
-    "fda3f793697acd377664e59987caa4b3a5c2118237e6c2889510a484a18b4ea3"
+    "0xfda3f793697acd377664e59987caa4b3a5c2118237e6c2889510a484a18b4ea3"
   );
 
   await nft.deployed();
