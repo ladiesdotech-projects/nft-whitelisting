33406186ab755c7285d76b2a32ae309f87864efc
emmaglorypraise <emmaglorypraise@gmail.com>
2022-05-12 13:55:56 +0100

    push changes

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,203 +1,112 @@
 import Head from "next/head";
-// import Link from "next/link";
+import Link from "next/link";
 import { NFT_CONTRACT_ADDRESS, abi } from "../constants";
 import { useEffect, useRef, useState  } from "react";
 import { ethers, providers, Contract } from "ethers";
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify'
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 
 
-export default function Home() 
-  const [walletConnected, setWalletConnected] = useState(false);
-  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
-  const web3ModalRef = useRef();
+export default function Home() {
 
-  const providerOptions = {
-    walletlink: {
-      package: CoinbaseWalletSDK,
-      options: {
-        appName: "Web 3 Modal Demo",
-        infuraId: "2007d5f12608499d9cbddfe532a9759b",
-      },
-    },
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        infuraId: "434d293815cd474081e89aefc46a5371",
-      },
-    },
-    binancechainwallet: {
-      package: true
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = async () => { 
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      toast.error("Make sure you are connected to Metamask");
+      return;
+    } else {
+      toast.success('Wallet exist!, we are ready to go!');
     }
-  };
+
+    const accounts = await ethereum.request({ method: 'eth_accounts'});
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      toast.success('Wallet connected!');
+      setCurrentAccount(account);
+    } else {
+      toast.error("Make sure you are connected to Metamask");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      toast.error("Biko...connect to Metamask");
+    }
+
+    try {
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+      toast.success('Wallet already connected:');
+      setCurrentAccount(accounts[0]);
+    } catch(err) {
+      console.log(err);
+      toast.error("Unable to connect to Metamask");
+    }
+   }
+
+  const disconnectWallet = () => {
+    toast.error("Disconnecting wallet from dApp");
+    refreshState()
+    setCurrentAccount(false)
+  }
+
+  const refreshState = () => {
+    setCurrentAccount(false)
+  }
+
+  const connectWalletButton =  () => {
+    return (
+      <li
+      onClick={connectWalletHandler}
+      className="nav-item "
+      style={{ cursor: "pointer" }}
+    >
+      <a
+        className="nav-link button contact"
+        style={{
+          "padding-left": "20px",
+          "padding-right": "20px",
+          color: "lightgray",
+        }}
+      >
+        Connect Wallet
+      </a>
+    </li>
+    )
+  }
+
+  const claimWhitelistButton =  () => {
+    return (
+        <li
+          // onClick={connectWalletHandler}
+          className="nav-item"
+          style={{ cursor: "pointer" }}
+        >
+          <a
+            className="nav-link button contact"
+            style={{
+              "padding-left": "20px",
+              "padding-right": "20px",
+              color: "lightgray",
+            }}
+          >
+            Claim Whitelist 
+          </a>
+        </li>
+    )
+  }
 
   useEffect(() => {
-    if (!walletConnected) {
-      web3ModalRef.current = new Web3Modal({
-        network: "rinkeby",
-        providerOptions: {},
-        disableInjectedProvider: false,
-        providerOptions 
-      });
-      connectWallet();
-    }
-  }, [walletConnected]);
-
-
-  const getProviderOrSigner = async (needSigner = false) => {
-    const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.Web3Provider(provider);
-
-    const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 4) {
-      window.alert("Change the network to Rinkeby");
-      throw new Error("Change network to Rinkeby");
-    }
-
-    if (needSigner) {
-      const signer = web3Provider.getSigner();
-      return signer;
-    }
-    return web3Provider;
-  };
-
-  // const addAddressToWhitelist = async () => {
-  //   try {
-  //     const signer = await getProviderOrSigner(true);
-  //     const whitelistContract = new Contract(
-  //       NFT_CONTRACT_ADDRESS, 
-  //       abi,
-  //       signer
-  //     );
-  //     const tx = await whitelistContract.addAddressToWhitelist();
-  //     setLoading(true);
-  //     await tx.wait();
-  //     setLoading(false);
-  //     await getNumberOfWhitelisted();
-  //     setJoinedWhitelist(true);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  // const getNumberOfWhitelisted = async () => {
-  //   try {
-  //     const provider = await getProviderOrSigner();
-  //    const whitelistContract = new Contract(
-  //       NFT_CONTRACT_ADDRESS,
-  //       abi,
-  //       provider
-  //     );
-  //     const _numberOfWhitelisted = await whitelistContract.numAddressesWhitelisted();
-  //     setNumberOfWhitelisted(_numberOfWhitelisted);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  // const checkIfAddressInWhitelist = async () => {
-  //   try {
-  //    const signer = await getProviderOrSigner(true);
-  //     const whitelistContract = new Contract(
-  //       NFT_CONTRACT_ADDRESS,
-  //       abi,
-  //       signer
-  //     );
-  //     const address = await signer.getAddress();
-  //     // console.log(address)
-  //     const _joinedWhitelist = await whitelistContract.whitelistedAddresses(
-  //       address
-  //     );
-  //     setJoinedWhitelist(_joinedWhitelist);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  
-
-  const connectWallet = async () => {
-    try {
-      await getProviderOrSigner();
-      setWalletConnected(true);
-
-      // checkIfAddressInWhitelist();
-      // getNumberOfWhitelisted();
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const renderButton = () => {
-    if (walletConnected) {
-      if (joinedWhitelist) {
-        return (
-          <div 
-          // className={styles.description}
-          >
-            Thanks for joining the Whitelist!
-          </div>
-        );
-      } else if (loading) {
-        return <button 
-        // className={styles.button}
-        >Loading...</button>;
-      } else {
-        return (
-          <button onClick={addAddressToWhitelist}
-          //  className={styles.button}
-           >
-            Join the Whitelist
-          </button>
-        );
-      }
-    } else {
-      return (
-        <li
-            className="nav-item heading-connect"
-            style={{ cursor: "pointer" }}
-            onClick={connectWallet}
-          >
-            <a
-              className="nav-link button contact"
-              style={{
-                "padding-left": "20px",
-                "padding-right": "20px",
-                color: "lightgray",
-              }}
-            >
-              Connect wallet
-            </a>
-          </li>
-      );
-    }
-  };
-
-
-  
-
-
-  // const connectWalletNavBtn = (
-  //   <li
-  //     className="nav-item heading-connect"
-  //     style={{ cursor: "pointer" }}
-  //     onClick={secondaryConnect}
-  //   >
-  //     <a
-  //       className="nav-link button contact"
-  //       style={{
-  //         "padding-left": "20px",
-  //         "padding-right": "20px",
-  //         color: "white",
-  //       }}
-  //     >
-  //       Connect wallet
-  //     </a>
-  //   </li>
-  // );
+    checkWalletIsConnected();
+  }, [])
 
   return (
     <div>
@@ -205,8 +114,107 @@ export default function Home()
         <title>Group 3 Web3Ladies!</title>
         <meta name="description" content="G3L Web3Ladies" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="./css/tailwind.css" />
       </Head>
-
+      <header>
+      <nav className="navbar navbar-expand-md navbar-light p-0 m-0">
+        <div className="container-fluid p-0 m-0">
+          <Link href="/">
+            <a className="navbar-brand fa">G3L Web3Ladies</a>
+          </Link>
+          <button
+            className="navbar-toggler text-light"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="toggle navigation"
+          >
+            <span className="navbar-toggler-icon text-light">
+              <i className="fas fa-bars"></i>
+            </span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul
+              className="navbar-nav"
+              style={{
+                width: "95%",
+                display: "flex",
+                "-webkit-box-pack": "end",
+                "-webkit-justify-content": "flex-end",
+                "-ms-flex-pack": "end",
+                "justify-content": "flex-end",
+              }}
+            >
+              <li className="nav-item"></li>
+              <li className="nav-item">
+                <Link href="/">
+                  <a
+                    className="nav-link"
+                    style={{ color: "rgb(134, 132, 132)" }}
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </Link>
+              </li>
+              {currentAccount ? (
+                  <li
+                    onClick={disconnectWallet}
+                    className="nav-item"
+                    style={{ cursor: "pointer" }}
+                    id="connectBtn"
+                >
+                  <a
+                    className="nav-link button contact"
+                    style={{
+                      "padding-left": "20px",
+                      "padding-right": "20px",
+                      color: "lightgray",
+                    }}
+                  >
+                    Disconnect wallet
+                  </a>
+                </li> 
+                ) : (
+                  <li
+                    onClick={connectWalletHandler}
+                    className="nav-item"
+                    style={{ cursor: "pointer" }}
+                    id="connectBtn"
+                  
+                  >
+                    <a
+                      className="nav-link button contact"
+                      style={{
+                        "padding-left": "20px",
+                        "padding-right": "20px",
+                        color: "lightgray",
+                      }}
+                    >
+                      Connect Wallet
+                    </a>
+                  </li>
+                )}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+      {/* main section starts here */}
       <div className="main">
         <div className="main-left">
           <h1 className="title">
@@ -219,18 +227,10 @@ export default function Home()
             {library ? tokenIds : "0"}
             /20 have been whitelisted
           </div> */}
-
-          {/* <div>{connected ? exploreBtn : connectWalletNavBtn}</div> */}
-          
-          <div 
-          // className={styles.description}
-          >
-            {numberOfWhitelisted} have already joined the Whitelist
+          <div className="description">
+          {currentAccount ? claimWhitelistButton() : connectWalletButton()}
           </div>
-          {renderButton()}
-
         </div>
-
         <div className="main-right">
           <img className="image" alt="Group 3 Web3Ladies!" src="./cryptodev.jpg" />
         </div>
