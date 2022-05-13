6f641ee0ee14522485149674cc1ba909186ffe9d
Fafemi Adeola <adeola5678@gmail.com>
2022-05-13 15:45:14 +0100

    updates

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,112 +1,140 @@
-import Head from "next/head";
-import Link from "next/link";
-import { NFT_CONTRACT_ADDRESS, abi } from "../constants";
-import { useEffect, useRef, useState  } from "react";
-import { ethers, providers, Contract } from "ethers";
+import Head from 'next/head'
+import Link from 'next/link'
+import { NFT_CONTRACT_ADDRESS, abi, config } from '../constants'
+import { useEffect, useState } from 'react'
+import { ethers, Contract } from 'ethers'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer, toast } from 'react-toastify'
-import Web3Modal from "web3modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
-
+import keccak256 from 'keccak256'
+import { MerkleTree } from 'merkletreejs'
 
 export default function Home() {
+  const [currentAccount, setCurrentAccount] = useState(null)
 
-  const [currentAccount, setCurrentAccount] = useState(null);
-
-  const checkWalletIsConnected = async () => { 
-    const { ethereum } = window;
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window
 
     if (!ethereum) {
-      toast.error("Make sure you are connected to Metamask");
-      return;
+      toast.error('Make sure you are connected to Metamask')
+      return
     } else {
-      toast.success('Wallet exist!, we are ready to go!');
+      toast.success('Wallet exist!, we are ready to go!')
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts'});
+    const accounts = await ethereum.request({ method: 'eth_accounts' })
 
     if (accounts.length !== 0) {
-      const account = accounts[0];
-      toast.success('Wallet connected!');
-      setCurrentAccount(account);
+      const account = accounts[0]
+      toast.success('Wallet connected!')
+      setCurrentAccount(account)
     } else {
-      toast.error("Make sure you are connected to Metamask");
+      toast.error('Make sure you are connected to Metamask')
     }
   }
 
   const connectWalletHandler = async () => {
-    const { ethereum } = window;
+    const { ethereum } = window
 
     if (!ethereum) {
-      toast.error("Biko...connect to Metamask");
+      toast.error('Connect to Metamask')
     }
 
     try {
-      const accounts = await ethereum.request({method: 'eth_requestAccounts'});
-      toast.success('Wallet already connected:');
-      setCurrentAccount(accounts[0]);
-    } catch(err) {
-      console.log(err);
-      toast.error("Unable to connect to Metamask");
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+      toast.success('Wallet already connected:')
+      setCurrentAccount(accounts[0])
+    } catch (err) {
+      console.log(err)
+      toast.error('Unable to connect to Metamask')
     }
-   }
+  }
 
   const disconnectWallet = () => {
-    toast.error("Disconnecting wallet from dApp");
     refreshState()
     setCurrentAccount(false)
+    toast.success('Wallet Disconnected')
   }
 
   const refreshState = () => {
     setCurrentAccount(false)
   }
 
-  const connectWalletButton =  () => {
+  const connectWalletButton = () => {
     return (
       <li
-      onClick={connectWalletHandler}
-      className="nav-item "
-      style={{ cursor: "pointer" }}
-    >
-      <a
-        className="nav-link button contact"
-        style={{
-          "padding-left": "20px",
-          "padding-right": "20px",
-          color: "lightgray",
-        }}
+        onClick={connectWalletHandler}
+        className="nav-item "
+        style={{ cursor: 'pointer' }}
       >
-        Connect Wallet
-      </a>
-    </li>
+        <a
+          className="nav-link button contact"
+          style={{
+            'padding-left': '20px',
+            'padding-right': '20px',
+            color: 'lightgray',
+          }}
+        >
+          Connect Wallet
+        </a>
+      </li>
     )
   }
 
-  const claimWhitelistButton =  () => {
+  const claimWhitelistButton = () => {
     return (
-        <li
-          // onClick={connectWalletHandler}
-          className="nav-item"
-          style={{ cursor: "pointer" }}
+      <li
+        onClick={(e) => onClickMint(e)}
+        className="nav-item"
+        style={{ cursor: 'pointer' }}
+      >
+        <a
+          className="nav-link button contact"
+          style={{
+            'padding-left': '20px',
+            'padding-right': '20px',
+            color: 'lightgray',
+          }}
         >
-          <a
-            className="nav-link button contact"
-            style={{
-              "padding-left": "20px",
-              "padding-right": "20px",
-              color: "lightgray",
-            }}
-          >
-            Claim Whitelist 
-          </a>
-        </li>
+          Claim Whitelist
+        </a>
+      </li>
     )
   }
 
   useEffect(() => {
-    checkWalletIsConnected();
+    checkWalletIsConnected()
   }, [])
+
+  const onClickMint = async (e) => {
+    e.preventDefault()
+    // get address and signer
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const accounts = await provider.listAccounts()
+      const signer = provider.getSigner()
+      // generate proof
+      const leaf = keccak256(accounts[0])
+      const leaves = config.addresses.map((x) => keccak256(x))
+      const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
+      const proof = tree.getHexProof(leaf)
+      const NFTContractInstance = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        abi,
+        signer
+      )
+      const claimTx = await NFTContractInstance.claim(
+        accounts[0],
+        'ipfs://unknown',
+        proof
+      )
+      const result = await claimTx.wait()
+      console.log(result)
+      toast.success('Successfully claimed NFT')
+    } catch (err) {
+      // console.error(err)
+      toast.error('Error claiming NFT')
+    }
+  }
 
   return (
     <div>
@@ -117,125 +145,129 @@ export default function Home() {
         <link rel="stylesheet" href="./css/tailwind.css" />
       </Head>
       <header>
-      <nav className="navbar navbar-expand-md navbar-light p-0 m-0">
-        <div className="container-fluid p-0 m-0">
-          <Link href="/">
-            <a className="navbar-brand fa">G3L Web3Ladies</a>
-          </Link>
-          <button
-            className="navbar-toggler text-light"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="toggle navigation"
-          >
-            <span className="navbar-toggler-icon text-light">
-              <i className="fas fa-bars"></i>
-            </span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul
-              className="navbar-nav"
-              style={{
-                width: "95%",
-                display: "flex",
-                "-webkit-box-pack": "end",
-                "-webkit-justify-content": "flex-end",
-                "-ms-flex-pack": "end",
-                "justify-content": "flex-end",
-              }}
+        <nav className="navbar navbar-expand-md navbar-light m-0 p-0">
+          <div className="container-fluid m-0 p-0">
+            <Link href="/">
+              <a className="navbar-brand fa">G3L Web3Ladies</a>
+            </Link>
+            <button
+              className="navbar-toggler text-light"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="toggle navigation"
             >
-              <li className="nav-item"></li>
-              <li className="nav-item">
-                <Link href="/">
-                  <a
-                    className="nav-link"
-                    style={{ color: "rgb(134, 132, 132)" }}
-                    aria-current="page"
-                  >
-                    Home
-                  </a>
-                </Link>
-              </li>
-              {currentAccount ? (
+              <span className="navbar-toggler-icon text-light">
+                <i className="fas fa-bars"></i>
+              </span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul
+                className="navbar-nav"
+                style={{
+                  width: '95%',
+                  display: 'flex',
+                  '-webkit-box-pack': 'end',
+                  '-webkit-justify-content': 'flex-end',
+                  '-ms-flex-pack': 'end',
+                  'justify-content': 'flex-end',
+                }}
+              >
+                <li className="nav-item"></li>
+                <li className="nav-item">
+                  <Link href="/">
+                    <a
+                      className="nav-link"
+                      style={{ color: 'rgb(134, 132, 132)' }}
+                      aria-current="page"
+                    >
+                      Home
+                    </a>
+                  </Link>
+                </li>
+                {currentAccount ? (
                   <li
                     onClick={disconnectWallet}
                     className="nav-item"
-                    style={{ cursor: "pointer" }}
+                    style={{ cursor: 'pointer' }}
                     id="connectBtn"
-                >
-                  <a
-                    className="nav-link button contact"
-                    style={{
-                      "padding-left": "20px",
-                      "padding-right": "20px",
-                      color: "lightgray",
-                    }}
-                  >
-                    Disconnect wallet
-                  </a>
-                </li> 
-                ) : (
-                  <li
-                    onClick={connectWalletHandler}
-                    className="nav-item"
-                    style={{ cursor: "pointer" }}
-                    id="connectBtn"
-                  
                   >
                     <a
                       className="nav-link button contact"
                       style={{
-                        "padding-left": "20px",
-                        "padding-right": "20px",
-                        color: "lightgray",
+                        'padding-left': '20px',
+                        'padding-right': '20px',
+                        color: 'lightgray',
+                      }}
+                    >
+                      Disconnect wallet
+                    </a>
+                  </li>
+                ) : (
+                  <li
+                    onClick={connectWalletHandler}
+                    className="nav-item"
+                    style={{ cursor: 'pointer' }}
+                    id="connectBtn"
+                  >
+                    <a
+                      className="nav-link button contact"
+                      style={{
+                        'padding-left': '20px',
+                        'padding-right': '20px',
+                        color: 'lightgray',
                       }}
                     >
                       Connect Wallet
                     </a>
                   </li>
                 )}
-            </ul>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
-    
-    <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
+        </nav>
+      </header>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       {/* main section starts here */}
       <div className="main">
         <div className="main-left">
-          <h1 className="title">
-          Welcome to Group 3 Web3Ladies!
-          </h1>
+          <h1 className="title">Welcome to Group 3 Web3Ladies!</h1>
           <div className="description">
-            Be one of the first to get Whitelisted for your Web3Ladies certificate. Connect your wallet to claim your spot.
+            Be one of the first to get Whitelisted for your Web3Ladies
+            certificate. Connect your wallet to claim your spot.
           </div>
           {/* <div className="description">
             {library ? tokenIds : "0"}
             /20 have been whitelisted
           </div> */}
           <div className="description">
-          {currentAccount ? claimWhitelistButton() : connectWalletButton()}
+            {currentAccount ? claimWhitelistButton() : connectWalletButton()}
           </div>
         </div>
         <div className="main-right">
-          <img className="image" alt="Group 3 Web3Ladies!" src="./cryptodev.jpg" />
+          <img
+            className="image"
+            alt="Group 3 Web3Ladies!"
+            src="./cryptodev.jpg"
+          />
         </div>
       </div>
-     
     </div>
-  );
+  )
 }
